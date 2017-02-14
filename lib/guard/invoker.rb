d18abf32d4c74a0424934769138b6ddcc69a2875
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-02-14 22:51:35 +0100

    Initial commit.

---
+++ lib/guard/invoker.rb
@@ -0,0 +1,117 @@
+require "guard/invoker/invoker_version"
+require 'guard/compat/plugin'
+
+module Guard
+  class Invoker < Plugin
+    # Initializes a Guard plugin.
+    # Don't do any work here, especially as Guard plugins get initialized even if they are not in an active group!
+    #
+    # @param [Hash] options the custom Guard plugin options
+    # @option options [Array<Guard::Watcher>] watchers the Guard plugin file watchers
+    # @option options [Symbol] group the group this Guard plugin belongs to
+    # @option options [Boolean] any_return allow any object to be returned from a watcher
+    #
+    def initialize(options = {})
+      say "Initialize invoker"
+      super
+    end
+
+    # Called once when Guard starts. Please override initialize method to init stuff.
+    #
+    # @raise [:task_has_failed] when start has failed
+    # @return [Object] the task result
+    #
+    def start
+      say "Starting invoker"
+      invoker_start
+    end
+
+    # Called when `stop|quit|exit|s|q|e + enter` is pressed (when Guard quits).
+    #
+    # @raise [:task_has_failed] when stop has failed
+    # @return [Object] the task result
+    #
+    def stop
+      say "Stopping invoker"
+      invoker_stop
+    end
+
+    # Called when `reload|r|z + enter` is pressed.
+    # This method should be mainly used for "reload" (really!) actions like reloading passenger/spork/bundler/...
+    #
+    # @raise [:task_has_failed] when reload has failed
+    # @return [Object] the task result
+    #
+    def reload
+      stop
+      start
+    end
+
+    # Called when just `enter` is pressed
+    # This method should be principally used for long action like running all specs/tests/...
+    #
+    # @raise [:task_has_failed] when run_all has failed
+    # @return [Object] the task result
+    #
+    def run_all
+      start
+    end
+
+    # Called on file(s) modifications that the Guard watches.
+    # 
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_change has failed
+    # 
+    def run_on_changes(paths)
+      reload
+    end
+
+    # Called on file(s) additions that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_additions has failed
+    # @return [Object] the task result
+    #
+    def run_on_additions(paths)
+      reload
+    end
+
+    # Called on file(s) modifications that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_modifications has failed
+    # @return [Object] the task result
+    #
+    def run_on_modifications(paths)
+      reload
+    end
+
+    # Called on file(s) removals that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_removals has failed
+    # @return [Object] the task result
+    #
+    def run_on_removals(paths)
+      reload
+    end
+
+    private
+
+    def invoker_start
+      system("#{executable} start")
+    end
+
+    def invoker_stop
+      system("#{executable} stop")
+    end
+
+    def executable
+      options.fetch(:executable) {'invoker'}
+    end
+
+    def say(what)
+      Guard::Compat::UI.info what, reset: true
+    end
+  end
+end
---
+++ lib/guard/invoker/invoker_version.rb
@@ -0,0 +1,5 @@
+module Guard
+  module InvokerVersion
+    VERSION = "0.1.0"
+  end
+end
